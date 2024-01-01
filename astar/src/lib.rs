cc3998ddd88417f5f87b2d5fe5cf0dde79e50a62
zepeh <zepeh5c@gmail.com>
2024-01-01 17:08:02 +0900

    코어 기능 추가
    - Vector2에 이동 관련 기능들 추가
    - 테스트 용 임시 기능 추가

--- astar/src/lib.rs
+++ astar/src/lib.rs
@@ -1,44 +1,69 @@
 use std::cell::RefCell;
 use std::rc::Rc;
+use std::clone::Clone;
+use std::cmp::Ordering;
 
 pub struct Vector2 {
-    x: i32,
-    y: i32
+    pub x: i32,
+    pub y: i32
 }
 
 impl Vector2 {
-    pub fn new(x: i32, y: i32) -> Vector2 {
-        Vector2 {
+    pub fn new(x: i32, y: i32) -> Self {
+        Self {
             x,
             y
         }
     }
 
-    pub fn up(&self) -> Vector2 {
-        Vector2 {
+    pub fn up(&self) -> Self {
+        Self {
+            x: self.x,
+            y: self.y -1
+        }
+    }
+
+    pub fn right(&self) -> Self {
+        Self {
+            x: self.x + 1,
+            y: self.y
+        }
+    }
+
+    pub fn down(&self) -> Self {
+        Self {
             x: self.x,
             y: self.y + 1
         }
     }
 
-    pub fn right(&self) -> Vector2 {
-        Vector2 {
+    pub fn left(&self) -> Self {
+        Self {
             x: self.x - 1,
             y: self.y
         }
     }
+}
 
-    pub fn down(&self) -> Vector2 {
-        Vector2 {
-            x: self.x,
-            y: self.y - 1
-        }
+impl Clone for Vector2 {
+    fn clone(&self) -> Self {
+        Vector2::new(self.x, self.y)
     }
+}
 
-    pub fn left(&self) -> Vector2 {
-        Vector2 {
-            x: self.x - 1,
-            y: self.y
+impl PartialEq for Vector2 {
+    fn eq(&self, other: &Self) -> bool {
+        self.x == other.x && self.y == other.y
+    }
+}
+
+impl PartialOrd for Vector2 {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        match self.x.partial_cmp(&other.x) {
+            // x 좌표가 같으면 y 좌표끼리 비교
+            Some(Ordering::Equal) => self.y.partial_cmp(&other.y),
+            // x 좌표가 다르면 그 결과를 반환
+            _ => None
         }
     }
 }
@@ -58,16 +83,43 @@ pub enum Tile {
 
 pub struct MazeMap {
     tiles: Vec<Tile>,
+    size: Vector2,
 }
 
 impl MazeMap {
-    pub fn generate(row: i32, col: i32) -> MazeMap {
+    pub fn generate(size: Vector2) -> MazeMap {
+        let mut tiles : Vec<Tile> = vec![];
+
+        for y in 0..size.y {
+            for x in 0 .. size.x {
+                tiles.push(Tile::Floor);
+            }
+        }
+
+        tiles[0] = Tile::StartingPoint;
+        *tiles.last_mut().unwrap() = Tile::Goal;
+
         MazeMap {
-            tiles: vec![]
+            tiles,
+            size
         }
     }
 
+    pub fn size(&self) -> Vector2 {
+        self.size.clone()
+    }
+
     pub fn get_tile(&self, pos: Vector2) -> &Tile {
-        &self.tiles[0]
+        let idx = self.get_idx_from_pos(&pos);
+        &self.tiles[idx]
+    }
+
+    pub fn set_tile(&mut self, pos: Vector2, tile: Tile) {
+        let idx = self.get_idx_from_pos(&pos);
+        self.tiles[idx] = tile;
+    }
+
+    fn get_idx_from_pos(&self, pos: &Vector2) -> usize {
+        (pos.y * self.size.x + pos.x) as usize
     }
 }
--- astar/src/main.rs
+++ astar/src/main.rs
@@ -1,30 +1,62 @@
-use astar::{MazeMap, Node, Vector2};
+use std::thread;
+use std::time::Duration;
+use astar::{MazeMap, Node, Tile, Vector2};
 
 struct Context {
     map: MazeMap,
-    agent_pos: Vector2
+    agent_pos: Vector2,
+    fps: i32,
+    is_running: bool
 }
 
 fn main() {
-    let fps = 30;
-
     let mut ctx = init();
 
-    loop {
+    while ctx.is_running {
         update(&mut ctx);
         render(&mut ctx);
     }
 }
 
 fn init() -> Context {
-    Context {
-        map: MazeMap::generate(10, 10),
-        agent_pos: Vector2::new(0, 0)
-    }
+    let context = Context {
+        map: MazeMap::generate(Vector2::new(20, 10)),
+        agent_pos: Vector2::new(0, 0),
+        fps: 30,
+        is_running: true
+    };
+    context
 }
 
 fn update(ctx: &mut Context) {
+    thread::sleep(Duration::from_secs(2));
+    ctx.agent_pos = ctx.agent_pos.right();
 }
 
-fn render(ctx: &mut Context) {
+fn render(ctx: &Context) {
+    let size = ctx.map.size();
+
+    for y in 0..size.y {
+        for x in 0..size.x {
+            let pos = Vector2::new(x, y);
+            if ctx.agent_pos == pos {
+                print!("{}", '&');
+            } else {
+                let tile = ctx.map.get_tile(pos);
+                print!("{}", render_tile(tile));
+            }
+        }
+        print!("\r\n");
+    }
+}
+
+fn render_tile(tile: &Tile) -> char {
+    match tile {
+        Tile::Blank => ' ',
+        Tile::Floor => '☐',
+        Tile::Goal => '✩',
+        Tile::StartingPoint => '@',
+        Tile::Wall => '◼',
+        _ => '?'
+    }
 }
